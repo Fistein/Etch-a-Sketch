866bfafb99a3a86650d4ce99c80ad9bd278f6cbc
Fistein <fistonikez@gmail.com>
2023-09-11 00:11:00 +0300

    Add index.html and main.css and script.js and styles.css

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
-let screen = document.querySelector('.grid-displays');
-screen.style.gridTemplateColumns = 'repeat(16,1fr)';
-screen.style.gridTemplateRows = 'repeat(16,1fr)';
 
-for(let i = 0; i < 256; i++){
-    let squares = document.createElement('div');
-    squares.style.backgroundColor = "#c5d401";
-    screen.insertAdjacentElement('beforeEnd',squares);
+function sketch(data){
+let screen = document.querySelector('.grid-displays');
+let gridz = screen.querySelectorAll('div');
+gridz.forEach((div) => div.remove());
+screen.style.gridTemplateColumns = `repeat(${data},1fr)`;
+screen.style.gridTemplateRows = `repeat(${data},1fr)`;;
+let area = data * data;
+for(let i = 0; i < area; i++){
+    let square = document.createElement('div');
+    square.addEventListener('mouseover', () => 
+    {
+       
+     square.style.backgroundColor = `RGB(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;  
+    });
+    square.style.backgroundColor = "#ffffff";
+    screen.insertAdjacentElement('beforeEnd',square);
 }
+
+}
+
+function adjustment(size){
+    let equal = size + 0;
+    if(size < 0 || size > 100){
+        console.log('Enter a number between 1 and 100') ;
+    }
+    else{
+        sketch(size);  
+    }
+    
+}
+
+const clickbutton = document.querySelector('.adjust');
+clickbutton.addEventListener('click', adjustment)
+
+
+
+let modal = document.getElementById('grid-form');
+let overlay = document.querySelector('#overlay');
+
+function Open_modal(){
+  modal.style.display = 'block';
+  overlay.style.display = 'block';
+}
+
+const set_button = document.querySelector('.send');
+set_button.addEventListener('click', () => {
+    
+    modal.style.display = 'none';
+    overlay.style.display = 'none';  
+        
+    });
+  
+let close_pop = document.querySelector('.close-btn');
+close_pop.addEventListener('click', () => {
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+});
